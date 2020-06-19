2fce6a31261e8a58d8a96805fdd6aceb37aef525
Mohammad Mehdi Mohsin <molvimohammadmehdi@gmail.com>
2020-06-19 21:48:08 +0500

    Enable Migration

---
+++ HMS.DataBase/HMSContext.cs
@@ -0,0 +1,24 @@
+﻿using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.DataBase
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext() :base("HMSConnectionString")
+        {
+
+        }
+
+        public DbSet<AccomodationType> AccomodationType { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackage { get; set; }
+        public DbSet<Accomodation> Accomodation { get; set; }
+        public DbSet<Booking> Booking { get; set; }
+
+    }
+}
